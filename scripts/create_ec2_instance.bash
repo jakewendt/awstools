a998cdf53a9aa9cff748ab2cb6cd288e7cc8727e
Jake <github@jakewendt.com>
2016-10-13 09:33:21 -0700

    Isolating previously used scripts from other projects. Added --profile option. Will remove from other repos when complete.

---
+++ scripts/create_ec2_instance.bash
@@ -0,0 +1,310 @@
+#!/usr/bin/env bash
+
+function usage(){
+	echo
+	echo "Start an EC2 instance on AWS"
+	echo
+	echo "Usage: (NO EQUALS SIGNS)"
+	echo
+	echo "`basename $0` [--profile STRING] [--image-id AMI] [--instance-type AMITYPE] [--key KEY_WITH_PATH] [--user-data USERDATAFILE] [--volume-size INTEGER] [--count INTEGER] [--NOT-DRY-RUN]"
+	echo
+	echo "--NOT-DRY-RUN is a boolean flag to ACTUALLY start instance (without, does not)"
+	echo
+	echo "Image IDs are region specific."
+	echo "They must exist in the region specified in the user's ~/.aws/config"
+	echo
+	echo "Defaults:"
+	echo
+	echo " image_id ....... ${image_id}"
+	echo " instance_type .. ${instance_type}"
+	echo " key ............ ${key} (the key and file base name NEED to be the same)"
+	echo " volume-size .... Default Image Volume Size"
+	echo " count .......... ${count}"
+	echo " profile ........ ${profile}"
+	echo
+	echo "Key Pairs can easily be create like ..."
+	echo "aws ec2 create-key-pair --key-name KEYNAME --query 'KeyMaterial' --output text > ~/.aws/KEYNAME.pem"
+	echo "Key Pairs are also region specific."
+	echo
+	echo "          vCPU   ECU  Memory(GiB) Linux/UNIX Usage (US West 2 - 20160705)"
+	echo "t2.nano     1    Var     0.5     \$0.0065 per Hour"
+	echo "t2.micro    1    Var     1       \$0.013 per Hour (Free Tier)"
+	echo "t2.small    1    Var     2       \$0.026 per Hour"
+	echo "t2.medium   2    Var     4       \$0.052 per Hour"
+	echo "t2.large    2    Var     8       \$0.104 per Hour"
+	echo "m3.medium   1     3      3.75    \$0.067 per Hour"
+	echo "m3.large    2     6.5    7.5     \$0.133 per Hour"
+	echo "m3.xlarge   4    13     15       \$0.266 per Hour"
+	echo "m3.2xlarge  8    26     30       \$0.532 per Hour"
+	echo
+	echo "Example:"
+	echo
+	echo "`basename $0` --key ~/.aws/JakeHuman.pem --user-data aws_start_suicidal_queue.user_data"
+	echo
+	exit
+}
+
+# initial image id "ami-f303fb93"
+#	image_id="ami-f13efe91"	#	HERV 7
+#	image_id="ami-d35494b3"	#	HERV 8
+#	image_id="ami-d05595b0"	#	HERV 9
+#	image_id="ami-e227e482"	#	HERV 10
+image_id="ami-665f9006"	#	HERV 11
+instance_type="t2.micro"
+#instance_type="t2.medium"
+key="~/.aws/KEYNAME.pem"
+volume_size=8
+dry_run="--dry-run"
+#	--user-data file://aws_start_1000genomes_processing.sh
+user_data=""
+block=""	#	for the volume size
+count=1
+profile="default"	#	placeholder to user option
+
+while [ $# -ne 0 ] ; do
+	#	Options MUST start with - or --.
+	case $1 in
+		--NOT-DRY-RUN)
+			dry_run=""; shift ;;
+		--count)	#	no short cuts here. count and only count.
+			shift; count=$1; shift ;;
+		-in*|--in*)
+			shift; instance_type=$1; shift ;;
+		-im*|--im*)
+			shift; image_id=$1; shift ;;
+		-k*|--k*)
+			shift; key=$1; shift ;;
+		-u*|--u*)
+			shift; user_data="--user-data file://$1"; shift ;;
+		-p*|--p*)
+			shift; profile=$1; shift ;;
+		-v*|--v*)
+			shift; block="--block-device-mappings DeviceName=/dev/xvda,Ebs={VolumeSize=${1},VolumeType=gp2}"; shift ;;
+#          VirtualName=string,DeviceName=string,Ebs={SnapshotId=string,VolumeSize=integer,DeleteOnTermination=boolean,VolumeType=string,Iops=integer,Encrypted=boolean},NoDevice=string ...
+		-h*|--h*)
+			usage ;;
+		--)	#	just -- is a common and explicit "stop parsing options" option
+			shift; break ;;
+		-*)
+			echo ; echo "Unexpected args from: ${*}"; usage ;;
+		*)
+			break ;;
+	esac
+done
+
+#       Basically, this is TRUE AND DO ...
+[ $# -ne 0 ] && usage
+
+
+key_name=${key%.*} # drop the shortest suffix match to ".*" (the .pem extension)
+key_name=${key_name##*/}	#	drop the longest prefix match to "*/" (the path)
+
+
+
+#	Get a subnet. Which one?
+#	Subnet can only have X number of ip addresses. (X is over 4000, fyi)
+
+#	select by vpcid. Which vpcid?
+#	sort by AvailableIpAddressCount?
+
+#	This should return that with the MOST available.
+subnets=`aws --profile $profile ec2 describe-subnets`
+echo $subnets
+#aws --profile $profile ec2 describe-subnets
+#{
+#    "Subnets": [
+#        {
+#            "VpcId": "vpc-0f333c6b", 
+#            "CidrBlock": "172.31.0.0/20", 
+#            "MapPublicIpOnLaunch": false, 
+#            "DefaultForAz": false, 
+#            "State": "available", 
+#            "AvailabilityZone": "us-east-1a", 
+#            "SubnetId": "subnet-356f0543", 
+#            "AvailableIpAddressCount": 4091
+#        }, 
+#        {
+#            "VpcId": "vpc-0f333c6b", 
+#            "CidrBlock": "172.31.16.0/20", 
+#            "MapPublicIpOnLaunch": false, 
+#            "DefaultForAz": false, 
+#            "State": "available", 
+#            "AvailabilityZone": "us-east-1b", 
+#            "SubnetId": "subnet-0bca4853", 
+#            "AvailableIpAddressCount": 4091
+#        }
+#    ]
+#}
+
+
+subnet_id=`echo $subnets | jq '.Subnets | sort_by(.AvailableIpAddressCount) | reverse[0].SubnetId' | tr -d '"'`
+echo $subnet_id
+
+
+
+#$ aws --profile $profile ec2 describe-images --image-ids ami-60b6c60a
+#{
+#    "Images": [
+#        {
+#            "VirtualizationType": "hvm", 
+#            "Name": "amzn-ami-hvm-2015.09.1.x86_64-gp2", 
+#            "Hypervisor": "xen", 
+#            "ImageOwnerAlias": "amazon", 
+#            "SriovNetSupport": "simple", 
+#            "ImageId": "ami-60b6c60a", 
+#            "State": "available", 
+#            "BlockDeviceMappings": [
+#                {
+#                    "DeviceName": "/dev/xvda", 
+#                    "Ebs": {
+#                        "DeleteOnTermination": true, 
+#                        "SnapshotId": "snap-a17f1036", 
+#                        "VolumeSize": 8, 
+#                        "VolumeType": "gp2", 
+#                        "Encrypted": false
+#                    }
+#                }
+#            ], 
+#            "Architecture": "x86_64", 
+#            "ImageLocation": "amazon/amzn-ami-hvm-2015.09.1.x86_64-gp2", 
+#            "RootDeviceType": "ebs", 
+#            "OwnerId": "137112412989", 
+#            "RootDeviceName": "/dev/xvda", 
+#            "CreationDate": "2015-10-29T18:17:23.000Z", 
+#            "Public": true, 
+#            "ImageType": "machine", 
+#            "Description": "Amazon Linux AMI 2015.09.1 x86_64 HVM GP2"
+#        }
+#    ]
+#}
+
+#	20160405 - Newest version of above
+#$ aws --profile $profile ec2 describe-images --image-ids ami-08111162
+#
+#{
+#    "Images": [
+#        {
+#            "VirtualizationType": "hvm", 
+#            "Name": "amzn-ami-hvm-2016.03.0.x86_64-gp2", 
+#            "Hypervisor": "xen", 
+#            "ImageOwnerAlias": "amazon", 
+#            "SriovNetSupport": "simple", 
+#            "ImageId": "ami-08111162", 
+#            "State": "available", 
+#            "BlockDeviceMappings": [
+#                {
+#                    "DeviceName": "/dev/xvda", 
+#                    "Ebs": {
+#                        "DeleteOnTermination": true, 
+#                        "SnapshotId": "snap-12c47a84", 
+#                        "VolumeSize": 8, 
+#                        "VolumeType": "gp2", 
+#                        "Encrypted": false
+#                    }
+#                }
+#            ], 
+#            "Architecture": "x86_64", 
+#            "ImageLocation": "amazon/amzn-ami-hvm-2016.03.0.x86_64-gp2", 
+#            "RootDeviceType": "ebs", 
+#            "OwnerId": "137112412989", 
+#            "RootDeviceName": "/dev/xvda", 
+#            "CreationDate": "2016-03-16T23:48:08.000Z", 
+#            "Public": true, 
+#            "ImageType": "machine", 
+#            "Description": "Amazon Linux AMI 2016.03.0 x86_64 HVM GP2"
+#        }
+#    ]
+#}
+
+
+vpcid=`aws --profile $profile ec2 describe-vpcs --filters "Name=isDefault,Values=true" | jq '.Vpcs[].VpcId' | tr -d '"'`
+
+echo "VPC ID: ${vpcid}"
+
+sg=`aws --profile $profile ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpcid,Name=description,Values=default VPC security group"`
+sgid=`echo $sg | jq '.SecurityGroups[].GroupId' | tr -d '"'`
+echo "Security Group Id: ${sgid}"
+
+echo "Checking for existing ssh access"
+ssh_access=`aws --profile $profile ec2 describe-security-groups --group-id ${sgid} --filters Name=group-name,Values=default Name=ip-permission.protocol,Values=tcp Name=ip-permission.from-port,Values=22 Name=ip-permission.to-port,Values=22 Name=ip-permission.cidr,Values='0.0.0.0/0' --query 'SecurityGroups[*].{Name:GroupName}'`
+if [ "$ssh_access" == "[]" ]; then
+	echo "Explicitly enable ssh access (port 22)"
+	aws --profile $profile ec2 authorize-security-group-ingress \
+		--protocol tcp --port 22 --cidr 0.0.0.0/0 \
+		--group-id $sgid
+else
+	echo "SSH Access already exists. Skipping."
+fi
+
+command="aws --profile $profile ec2 run-instances ${dry_run} ${block}
+	--count ${count}
+	--image-id ${image_id}
+	--instance-type ${instance_type}
+	--key-name ${key_name}
+	--subnet-id ${subnet_id}
+	--associate-public-ip-address
+	--instance-initiated-shutdown-behavior terminate
+	--iam-instance-profile Name='ec2_processor' ${user_data}"
+
+#	--user-data file://aws_start_1000genomes_processing.sh
+# --block-device-mappings '[{"DeviceName":"/dev/xvda", "Ebs":{"VolumeSize":100,"VolumeType":"gp2"}}]'
+#	--block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":'${volume_size}',"VolumeType":"gp2"}}]'
+# --query 'Instances[].InstanceId'
+
+#	'ec2_processor' was "ec2_processor". Double quotes versus single quotes matter???? Nope.
+
+#	Double quotes preserve newlines (if they weren't escaped)
+echo "$command"
+instance=`$command`
+echo "$instance"
+
+instance_ids=`echo "$instance" | jq '.Instances[].InstanceId' | tr -d '"'`
+echo $instance_ids
+
+
+
+
+#$ aws --profile $profile ec2 describe-instances
+#{
+#    "Reservations": []
+#}
+
+
+command="aws --profile $profile ec2 describe-instances
+	--query 'Reservations[0].Instances[].PublicIpAddress'
+	--instance-ids $instance_ids"
+echo
+echo $command
+echo
+echo "ssh -i ${key} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@#.#.#.#"
+echo
+
+
+
+
+#	Seems that it may be next to impossible to select the latest
+#	Amazon Linux AMI programmatically
+
+#	rather than [] suffix, use map(select()) so output is an array.
+
+#	images=`aws --profile $profile ec2 describe-images`
+#$ echo $images | jq '.Images[] | length'
+#66401
+#$ echo $images | jq '.Images | map(select( .VirtualizationType == "hvm" )) | length'
+#22494
+#$ echo $images | jq '.Images | map(select( .VirtualizationType == "hvm" )) | map(select( .RootDeviceType == "ebs" )) | length'
+#21705
+#$ echo $images | jq '.Images | map(select( .VirtualizationType == "hvm" )) | map(select( .RootDeviceType == "ebs" )) | map(select( .Architecture == "x86_64" )) | length'
+#21087
+#$ echo $images | jq '.Images | map(select( .VirtualizationType == "hvm" )) | map(select( .RootDeviceType == "ebs" )) | map(select( .Architecture == "x86_64" ))  | map(select( .ImageOwnerAlias == "amazon" )) | length'
+#852
+
+
+#	Rather than jq, can also use python to pick from json
+#		line=`echo $message | python -c \
+#			'import sys, json; print json.load(sys.stdin)["Messages"][0]["Body"]'`
+#		echo $line
+#
+#		handle=`echo $message | python -c \
+#			'import sys, json; print json.load(sys.stdin)["Messages"][0]["ReceiptHandle"]'`
+#		echo $handle
---
+++ scripts/create_mariadb_instance.bash
@@ -0,0 +1,302 @@
+#!/usr/bin/env bash
+
+function usage(){
+	echo
+	echo "Start a MariaDB instance on AWS"
+	echo
+	echo "Usage: (NO EQUALS SIGNS)"
+	echo
+	echo "`basename $0` --password PASSWORD [--username USERNAME] [--profile STRING]"
+	echo
+	echo "Defaults:"
+	echo
+	echo " username .. $username"
+	echo " password .. "
+	echo " profile ... $profile"
+	echo
+	echo "aws/create_mariadb_instance.bash --username IAmHuman --password ******"
+	echo "cp awsqueue.cnf ~/.awsqueue.cnf"
+	echo "chmod 400 ~/.awsqueue.cnf"
+	echo "alias awsdb='mysql --defaults-file=~/.awsqueue.cnf'"
+	echo "alias awsq='mysql_queue.bash --defaults_file ~/.awsqueue.cnf'"
+	echo "awsq create"
+	echo "awsq push 'sleep 1 && echo \"I was sleeping!\" '"
+	echo "awsq list"
+	echo "awsq start"
+	echo "awsq list"
+	echo
+	exit
+}
+
+username='MyAWSUser'
+password=''
+profile='default'
+
+while [ $# -ne 0 ] ; do
+	#	Options MUST start with - or --.
+	case $1 in
+		-u*|--u*)
+			shift; username=$1; shift ;;
+		-pa*|--pa*)
+			shift; password=$1; shift ;;
+		-pr*|--pr*)
+			shift; profile=$1; shift ;;
+		-h*|--h*)
+			usage ;;
+		--)	#	just -- is a common and explicit "stop parsing options" option
+			shift; break ;;
+		-*)
+			echo ; echo "Unexpected args from: ${*}"; usage ;;
+		*)
+			break;;
+	esac
+done
+
+#       Basically, this is TRUE AND DO ...
+[ -z $password ] && usage
+
+db_instance_identifier='QueueDbInstanceId'
+
+
+#	Check if database with this id exists already.
+#	Basically
+#if [ `aws --profile $profile rds describe-db-instances | jq '.DBInstances | select(.DBInstanceIdentifier == "${db_instance_identifier}") | length'` -gt 0 ] ; then
+#	echo -e "You've already got a Queue DB setup. You probably don't want to do this.\n"
+#	exit
+#fi
+#
+#	If there aren't any, jq crashes
+#	
+
+
+#	If not found, the following will be printed on the STDERR. I don't need it, so to dev null.
+#	A client error (DBInstanceNotFound) occurred when calling the DescribeDBInstances operation: DBInstance QueueDbInstanceId not found.
+qdbi=`aws --profile $profile rds describe-db-instances --db-instance-identifier ${db_instance_identifier} 1> /dev/null 2>&1`
+#	qdbi should be blank, so storing it is unnecessary.
+status=$? 	#	0 when it exists, 255 when it doesn't
+if [ $status -eq 0 ] ; then
+	echo "${db_instance_identifier} already exists. Stopping."
+	exit $status
+fi
+
+#	Basically, don't overwrite the cnf file if a db exists.
+
+cat <<-EOF > awsqueue.cnf
+[mysql]
+user=$username
+password=$password
+database=QueueDbName
+port=3306
+host=
+EOF
+chmod 600 awsqueue.cnf
+
+
+vpcid=`aws --profile $profile ec2 describe-vpcs --filters "Name=isDefault,Values=true" | jq '.Vpcs[].VpcId' | tr -d '"'`
+
+echo "VPC ID: ${vpcid}"
+
+sg=`aws --profile $profile ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpcid,Name=description,Values=default VPC security group"`
+#echo "Security Group: ${sg}"
+sgid=`echo $sg | jq '.SecurityGroups[].GroupId' | tr -d '"'`
+echo "Security Group Id: ${sgid}"
+
+#echo "Explicitly enable ssh access (port 22)"
+#aws --profile $profile ec2 authorize-security-group-ingress \
+#	--protocol tcp --port 22 --cidr 0.0.0.0/0 \
+#	--group-id $sgid
+
+echo "Checking for existing external access"
+ext_access=`aws --profile $profile ec2 describe-security-groups --group-id ${sgid} --filters Name=group-name,Values=default Name=ip-permission.protocol,Values=tcp Name=ip-permission.from-port,Values=3306 Name=ip-permission.to-port,Values=3306 Name=ip-permission.cidr,Values='0.0.0.0/0' --query 'SecurityGroups[*].{Name:GroupName}'`
+
+if [ "$ext_access" == "[]" ]; then
+	echo "Explicitly enable mysql/mariadb access (port 3306)"
+	aws --profile $profile ec2 authorize-security-group-ingress \
+		--protocol tcp --port 3306 --cidr 0.0.0.0/0 \
+		--group-id $sgid
+else
+	echo "Access already exists. Skipping."
+fi
+
+db_subnet_group_name='DbSubnetGroupName'
+
+subnet_ids=`aws --profile $profile ec2 describe-subnets | jq '.Subnets[].SubnetId' | paste -s -d' ' | tr -d '"'`
+echo "Subnet Ids: ${subnet_ids}"
+
+subnetgroup=`aws --profile $profile rds create-db-subnet-group --db-subnet-group-name ${db_subnet_group_name} --db-subnet-group-description DbSubnetGroupDescription --subnet-ids ${subnet_ids}`
+echo $subnetgroup
+
+
+aws --profile $profile rds create-db-instance \
+	--publicly-accessible \
+	--engine mariadb \
+	--allocated-storage 5 \
+	--db-instance-class db.t2.micro \
+	--db-instance-identifier ${db_instance_identifier} \
+	--master-username $username \
+	--master-user-password $password \
+	--db-name QueueDbName \
+	--db-subnet-group-name ${db_subnet_group_name}
+
+#{
+#    "DBInstance": {
+#        "PubliclyAccessible": true, 
+#        "MasterUsername": "myawsuser", 
+#        "MonitoringInterval": 0, 
+#        "LicenseModel": "general-public-license", 
+#        "VpcSecurityGroups": [
+#            {
+#                "Status": "active", 
+#                "VpcSecurityGroupId": "sg-6f7e0717"
+#            }
+#        ], 
+#        "CopyTagsToSnapshot": false, 
+#        "OptionGroupMemberships": [
+#            {
+#                "Status": "in-sync", 
+#                "OptionGroupName": "default:mariadb-10-0"
+#            }
+#        ], 
+#        "PendingModifiedValues": {
+#            "MasterUserPassword": "****"
+#        }, 
+#        "Engine": "mariadb", 
+#        "MultiAZ": false, 
+#        "DBSecurityGroups": [], 
+#        "DBParameterGroups": [
+#            {
+#                "DBParameterGroupName": "default.mariadb10.0", 
+#                "ParameterApplyStatus": "in-sync"
+#            }
+#        ], 
+#        "AutoMinorVersionUpgrade": true, 
+#        "PreferredBackupWindow": "03:14-03:44", 
+#        "DBSubnetGroup": {
+#            "Subnets": [
+#                {
+#                    "SubnetStatus": "Active", 
+#                    "SubnetIdentifier": "subnet-186dee40", 
+#                    "SubnetAvailabilityZone": {
+#                        "Name": "us-east-1b"
+#                    }
+#                }, 
+#                {
+#                    "SubnetStatus": "Active", 
+#                    "SubnetIdentifier": "subnet-abcea5dd", 
+#                    "SubnetAvailabilityZone": {
+#                        "Name": "us-east-1a"
+#                    }
+#                }
+#            ], 
+#            "DBSubnetGroupName": "queuedbsubnetgroupname", 
+#            "VpcId": "vpc-a6434cc2", 
+#            "DBSubnetGroupDescription": "queuedbsubnetgroupdescription", 
+#            "SubnetGroupStatus": "Complete"
+#        }, 
+#        "ReadReplicaDBInstanceIdentifiers": [], 
+#        "AllocatedStorage": 5, 
+#        "BackupRetentionPeriod": 1, 
+#        "DBName": "queuedbname", 
+#        "PreferredMaintenanceWindow": "fri:06:50-fri:07:20", 
+#        "DBInstanceStatus": "creating", 
+#        "EngineVersion": "10.0.17", 
+#        "StorageType": "standard", 
+#        "DbiResourceId": "db-5BZYRDENNAEY66UWAMOECGSOCM", 
+#        "CACertificateIdentifier": "rds-ca-2015", 
+#        "StorageEncrypted": false, 
+#        "DBInstanceClass": "db.t2.micro", 
+#        "DbInstancePort": 0, 
+#        "DBInstanceIdentifier": "queuedbinstanceid"
+#    }
+#}
+
+#	WAIT for it to start to get endpoints
+
+
+
+
+#	aws --profile $profile rds describe-db-instances
+
+
+
+#	aws --profile $profile rds describe-db-instances --query "DBInstances[].Endpoint"
+#[
+#    {
+#        "Port": 3306, 
+#        "Address": "queuedbinstanceid.c8xrmj8azb5r.us-east-1.rds.amazonaws.com"
+#    }
+#]
+
+#	mysql --user myawsuser --password --port 3306 --host queuedbinstanceid.c8xrmj8azb5r.us-east-1.rds.amazonaws.com
+#
+#
+#				SUCCESS!!!!
+#
+#
+
+
+
+
+
+aws --profile $profile rds describe-db-instances \
+	--db-instance-identifier ${db_instance_identifier} \
+	--query "DBInstances[].Endpoint"
+
+
+
+
+
+
+
+#	If need to delete ...
+#	aws --profile $profile rds delete-db-instance --db-instance-identifier queuedbinstanceid --skip-final-snapshot
+#	Deleting can take several minutes
+
+
+
+
+
+#	To simplify usage, ~/.my.cnf could be created to include the database specs.
+#	Probably a good idea to "chmod 600 ~/.my.cnf"
+#	Then simple call "mysql --defaults-group-suffix=awsqueue"
+#[mysqlawsqueue]
+#	host
+#	port
+#	user
+#	password
+#	database
+#
+#	This cnf file overwrites settings so last setting sticks.
+#	eg
+#		database=overwritten_db
+#		database=used_db
+#
+
+#	Perhaps create an alias if you're really into it
+#	alias mysql_awsqueue="mysql --defaults-group-suffix=awsqueue"
+
+#	Or a separate defaults file could be created altogether. --defaults-file=HOST.cnf
+#	(I like this option. Its isolated and can be created and destroyed with the database)
+
+#	One could also use the encrypted version created by mysql_config_editor
+#	if you are using >5.6, I think. (~/.mylogin.cnf)
+#	Don't be fooled though. The file is encrypted, but the source code has the key.
+#	I could easily modify and recompile the source and have the password.
+#	IT IS NOT SECURE. Only fractionally moreso than ~/.my.cnf
+
+
+echo "Running the following in a few minutes to get the hostname."
+
+echo "aws --profile $profile rds describe-db-instances --db-instance-identifier ${db_instance_identifier} --query \"DBInstances[].Endpoint\""
+
+echo "Then add the address to the host line in the awsqueue.cnf file."
+
+echo "Then connect simply via ..."
+
+echo "mysql --defaults-file=awsqueue.cnf"
+
+echo "Perhaps, as I do, copy this awsqueue.cnf to ~/.awsqueue.cnf"
+echo "Then, create an alias \"alias awsdb='mysql --defaults-file=~/.awsqueue.cnf'\""
+echo "Then access is as simple as \"awsdb\""
+
+
